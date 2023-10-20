cf27dac6a489b41fa671ba182f290403d9192c55
HAPPY <ghelonicomaligaya111@gmail.com>
2023-10-20 22:51:58 +0800

    update: journey

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -9,8 +9,30 @@ const Journey = () => {
 
     return (
         <div className="journey-page" id='journey'>
-            <h1 ref={ref}
-                style={slideAnim(isInView, 2)} >JOURNEY</h1>
+            <div className='journey-left'>
+                <h2 ref={ref}
+                    style={slideAnim(isInView, 2)} >My Journey</h2>
+            </div>
+            <div className='journey-right'>
+                <div className='journey-right-inner'>
+                    <div className='journey-right-inner-details'>
+                        <span className='dot'></span>
+                        <div className='journey-right-inner-details-texts'>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                        </div>
+                        <span className='dot'></span>
+                        <div className='journey-right-inner-details-texts'>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
