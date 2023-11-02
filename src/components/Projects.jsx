daa40e3f316e55f3ab5ea9c311c1c4581c24b94b
HAPPY <ghelonicomaligaya111@gmail.com>
2023-11-02 22:03:08 +0800

    projects: update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,14 @@ import '../css/projects.css'
 const Projects = () => {
     return (
         <div className='projects-page' id='projects'>
-            <div className='prj-inner'>
-                
+            <h1>Projects</h1>
+            <div className='prj-inner-grid'>
+                <div className='prj-inner-left'>
+                    <h3>left</h3>
+                </div>
+                <div className='prj-inner-right'>
+                    <h3>right</h3>
+                </div>
             </div>
         </div>
     )
