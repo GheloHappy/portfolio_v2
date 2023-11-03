051fa77b8586f8a549cba4b49fb1e1b07c8a9e12
HAPPY <ghelonicomaligaya111@gmail.com>
2023-11-04 01:33:31 +0800

    imagekit links update

--- src/components/links/ImageLinks.jsx
+++ src/components/links/ImageLinks.jsx
@@ -1,46 +1,46 @@
 const ImageLinks = {
-    //languages
-    css: 'https://ik.imagekit.io/ghelowee/languages/css.webp?updatedAt=1699030682439',
-    html: 'https://ik.imagekit.io/ghelowee/languages/html.webp?updatedAt=1699030681312',
-    js: 'https://ik.imagekit.io/ghelowee/languages/js.webp?updatedAt=1699030681725',
-    java: 'https://ik.imagekit.io/ghelowee/languages/java.webp?updatedAt=1699030681591',
-    csharp: 'https://ik.imagekit.io/ghelowee/languages/csharp.webp?updatedAt=1699030681573',
-    php: 'https://ik.imagekit.io/ghelowee/languages/php.webp?updatedAt=1699030681605',
-    kotlin: 'https://ik.imagekit.io/ghelowee/languages/kotlin.webp?updatedAt=1699030681881',
-    xml: 'https://ik.imagekit.io/ghelowee/languages/xml.webp?updatedAt=1699030683804',
-    //frameworks
-    node: 'https://ik.imagekit.io/ghelowee/frameworks/node.webp?updatedAt=1699030677389',
-    reactjs: 'https://ik.imagekit.io/ghelowee/frameworks/reactjs.webp?updatedAt=1699030677361',
-    tailwind: 'https://ik.imagekit.io/ghelowee/frameworks/tailwindcss.webp?updatedAt=1699030675812',
-    laravel: 'https://ik.imagekit.io/ghelowee/frameworks/laravel.webp?updatedAt=1699030673116',
-    netcore: 'https://ik.imagekit.io/ghelowee/frameworks/netcore.webp?updatedAt=1699030672222',
-    asp: 'https://ik.imagekit.io/ghelowee/frameworks/asp.webp?updatedAt=1699030672668',
-    //databases
-    sqlserver: 'https://ik.imagekit.io/ghelowee/databases/sqlserver.webp?updatedAt=1699030672675',
-    postgre: 'https://ik.imagekit.io/ghelowee/databases/postgre.webp?updatedAt=1699030673108',
-    sqlite: 'https://ik.imagekit.io/ghelowee/databases/sqlite.webp?updatedAt=1699030672677',
-    mysql: 'https://ik.imagekit.io/ghelowee/databases/mysql.webp?updatedAt=1699030673142',
-    mongodb: 'https://ik.imagekit.io/ghelowee/databases/mongodb.webp?updatedAt=1699030673112',
+    //Portfolio/languages
+    css: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/css.webp?updatedAt=1699030682439',
+    html: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/html.webp?updatedAt=1699030681312',
+    js: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/js.webp?updatedAt=1699030681725',
+    java: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/java.webp?updatedAt=1699030681591',
+    csharp: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/csharp.webp?updatedAt=1699030681573',
+    php: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/php.webp?updatedAt=1699030681605',
+    kotlin: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/kotlin.webp?updatedAt=1699030681881',
+    xml: 'https://ik.imagekit.io/ghelowee/Portfolio/languages/xml.webp?updatedAt=1699030683804',
+    //Portfolio/frameworks
+    node: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/node.webp?updatedAt=1699030677389',
+    reactjs: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/reactjs.webp?updatedAt=1699030677361',
+    tailwind: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/tailwindcss.webp?updatedAt=1699030675812',
+    laravel: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/laravel.webp?updatedAt=1699030673116',
+    netcore: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/netcore.webp?updatedAt=1699030672222',
+    asp: 'https://ik.imagekit.io/ghelowee/Portfolio/frameworks/asp.webp?updatedAt=1699030672668',
+    //Portfolio/databases
+    sqlserver: 'https://ik.imagekit.io/ghelowee/Portfolio/databases/sqlserver.webp?updatedAt=1699030672675',
+    postgre: 'https://ik.imagekit.io/ghelowee/Portfolio/databases/postgre.webp?updatedAt=1699030673108',
+    sqlite: 'https://ik.imagekit.io/ghelowee/Portfolio/databases/sqlite.webp?updatedAt=1699030672677',
+    mysql: 'https://ik.imagekit.io/ghelowee/Portfolio/databases/mysql.webp?updatedAt=1699030673142',
+    mongodb: 'https://ik.imagekit.io/ghelowee/Portfolio/databases/mongodb.webp?updatedAt=1699030673112',
     //others
-    nginx: 'https://ik.imagekit.io/ghelowee/ide/nginx.webp?updatedAt=1699030677517',
-    cloudflare: 'https://ik.imagekit.io/ghelowee/ide/cloudflare.webp?updatedAt=1699030677671',
-    androidstudio: 'https://ik.imagekit.io/ghelowee/ide/androidstudio.webp?updatedAt=1699030676071',
-    vs: 'https://ik.imagekit.io/ghelowee/ide/vs.webp?updatedAt=1699030679829',
-    vscode: 'https://ik.imagekit.io/ghelowee/ide/vscode.webp?updatedAt=1699030680566',
-    eclipse: 'https://ik.imagekit.io/ghelowee/ide/eclipse.webp?updatedAt=1699030677662',
-    docker: 'https://ik.imagekit.io/ghelowee/ide/docker.webp?updatedAt=1699030676347',
-    linux: 'https://ik.imagekit.io/ghelowee/ide/linux.webp?updatedAt=1699030677366',
-    proxmox: 'https://ik.imagekit.io/ghelowee/ide/proxmox.webp?updatedAt=1699030677668',
-    windows: 'https://ik.imagekit.io/ghelowee/ide/windows.webp?updatedAt=1699030680490',
+    nginx: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/nginx.webp?updatedAt=1699030677517',
+    cloudflare: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/cloudflare.webp?updatedAt=1699030677671',
+    androidstudio: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/androidstudio.webp?updatedAt=1699030676071',
+    vs: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/vs.webp?updatedAt=1699030679829',
+    vscode: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/vscode.webp?updatedAt=1699030680566',
+    eclipse: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/eclipse.webp?updatedAt=1699030677662',
+    docker: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/docker.webp?updatedAt=1699030676347',
+    linux: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/linux.webp?updatedAt=1699030677366',
+    proxmox: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/proxmox.webp?updatedAt=1699030677668',
+    windows: 'https://ik.imagekit.io/ghelowee/Portfolio/ide/windows.webp?updatedAt=1699030680490',
 
-    //Projects
-    b2b_converter: 'https://ik.imagekit.io/ghelowee/projects/b2b_converter/main_form.webp?updatedAt=1699030684653',
-    barcode_system: 'https://ik.imagekit.io/ghelowee/projects/barcode_system/tab_login.webp?updatedAt=1699030685329',
-    it_monitoring: 'https://ik.imagekit.io/ghelowee/projects/it_monitoring/requests_page.webp?updatedAt=1699030685954',
-    timekeeping: 'https://ik.imagekit.io/ghelowee/projects/timekeeping/timekeeping.webp?updatedAt=1699030687822',
-    timekeeping_maintenance: 'https://ik.imagekit.io/ghelowee/projects/timekeeping_maintenance/reports.webp?updatedAt=1699030688985',
-    van_sales: 'https://ik.imagekit.io/ghelowee/projects/van_sales/login.webp?updatedAt=1699030689729',
-    warehouse_del_sched: 'https://ik.imagekit.io/ghelowee/projects/warehouse_del_sched_system/main_form.webp?updatedAt=1699030690739'
+    //Portfolio/projects
+    b2b_converter: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/b2b_converter/main_form.webp?updatedAt=1699030684653',
+    barcode_system: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/barcode_system/tab_login.webp?updatedAt=1699030685329',
+    it_monitoring: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/it_monitoring/requests_page.webp?updatedAt=1699030685954',
+    timekeeping: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/timekeeping/timekeeping.webp?updatedAt=1699030687822',
+    timekeeping_maintenance: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/timekeeping_maintenance/reports.webp?updatedAt=1699030688985',
+    van_sales: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/van_sales/login.webp?updatedAt=1699030689729',
+    warehouse_del_sched: 'https://ik.imagekit.io/ghelowee/Portfolio/projects/warehouse_del_sched_system/main_form.webp?updatedAt=1699030690739'
 }
 
 export default ImageLinks;
