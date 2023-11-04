dc11eb194a9b239adf3875b8297616f39fe1deea
HAPPY <ghelonicomaligaya111@gmail.com>
2023-11-05 00:00:12 +0800

    update: responsive projects

--- src/components/infos/ProjectsInfo.jsx
+++ src/components/infos/ProjectsInfo.jsx
@@ -4,48 +4,56 @@ export const projects_details = [
     {
         label: "B2B Converter",
         img: ImageLinks.b2b_converter,
+        img_mobile: ImageLinks.b2b_converter_mobile,
         summary: "Converts .csv, .xml files from Client Portals or Iconnect for directly processing of Purchase Order",
         stacks: [ImageLinks.vs, ImageLinks.csharp, ImageLinks.sqlserver]
     },
     {
         label: "Barcode System",
         img: ImageLinks.barcode_system,
+        img_mobile: ImageLinks.barcode_system_mobile,
         summary: "Crafted to streamline the monitoring of inbound and outbound shipments, as well as inventory status. This system leverages tablets and barcode scanners connected via both Wi-Fi and local networks, complemented by an API integration. Additionally, a dedicated desktop application extends its functionality to remote warehouses beyond the primary facility.",
         stacks: [ImageLinks.androidstudio, ImageLinks.java, ImageLinks.kotlin, ImageLinks.node, ImageLinks.vscode, ImageLinks.vs, ImageLinks.csharp, ImageLinks.sqlserver]
     },
     {
         label: "IT Monitoring",
         img: ImageLinks.it_monitoring,
+        img_mobile: ImageLinks.it_monitoring_mobile,
         summary: "A comprehensive fullstack web application for monitoring the inbound and outbound inventory within the IT department, including items such as printers, mice, keyboards, and more. The app also facilitates request creation and tracking for various forms and items.",
         stacks: [ImageLinks.vscode, ImageLinks.reactjs, ImageLinks.node, ImageLinks.sqlserver]
     },
     {
         label: "OS (Office Sales) Web",
         img: ImageLinks.os_web,
+        img_mobile: ImageLinks.os_web_mobile,
         summary: "An exclusive Fullstack E-Commerce app tailored specifically for employees. It enables employees to effortlessly add products to their cart from various company databases, streamlining their shopping experience.",
         stacks: [ImageLinks.vscode, ImageLinks.reactjs, ImageLinks.node, ImageLinks.sqlserver]
     },
     {
         label: "Timekeeping",
         img: ImageLinks.timekeeping,
+        img_mobile: ImageLinks.timekeeping_mobile,
         summary: "A desktop application that efficiently tracks employee attendance, offering the choice of either barcode ID or biometric verification for clocking in and out.",
         stacks: [ImageLinks.vs, ImageLinks.csharp, ImageLinks.sqlserver]
     },
     {
         label: "Timekeeping Maintenance",
         img: ImageLinks.timekeeping_maintenance,
+        img_mobile: ImageLinks.timekeeping_maintenance_mobile,
         summary: "A desktop application designed for HR professionals to effectively manage timekeeping tasks. This app allows HR employees to create and maintain employee records and schedules, view or export attendance records, and update leave requests and schedules.",
         stacks: [ImageLinks.vs, ImageLinks.csharp, ImageLinks.sqlserver]
     },
     {
         label: "Van Sales",
         img: ImageLinks.van_sales,
+        img_mobile: ImageLinks.van_sales_mobile,
         summary: "A Sales Force Automation (SFA) app developed to facilitate paperless sales transactions conducted from delivery vans, using tablets paired with Bluetooth printers for seamless and eco-friendly order processing.",
         stacks: [ImageLinks.eclipse, ImageLinks.java, ImageLinks.sqlite, ImageLinks.sqlserver]
     },
     {
         label: "Warehouse Delivery Schedule System",
         img: ImageLinks.warehouse_del_sched,
+        img_mobile: ImageLinks.warehouse_del_sched_mobile,
         summary: "Desktop application designed to streamline the management of invoices, simplifying the manual assignment of delivery trucks or wing vans and optimizing the routing of deliveries for a smoother and more efficient process.",
         stacks: [ImageLinks.vs, ImageLinks.csharp, ImageLinks.sqlserver]
     },
