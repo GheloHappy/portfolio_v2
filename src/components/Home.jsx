7e102bd37a9c79dfdfe0800b1d07379fae1cb4c6
HAPPY <ghelonicomaligaya111@gmail.com>
2023-11-04 23:13:09 +0800

    removed fade animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,14 @@ const Home = () => {
         <div className="home-page" id='home'>
             <div className='left'>
                 <div className='welcome'
-                    variants={fadeAnim(0.5)}
-                    initial='initial'
-                    animate='animate'>
+                    >
                     <h3>Hi there! I'm</h3>
                     <span>GHELONICO </span>
                     <span className='last-name'>MALIGAYA</span>
                     <h3>Software Developer</h3>
                 </div>
                 <div className='power-mobile' variants={fadeAnim(1)}
-                    initial='initial'
-                    animate='animate'>
+                    >
                     <p>Powered by</p>
                     {/* <img src={reactjs} className='mobile-logo' alt='react Logo' /> */}
                     <img src={ImageLinks.reactjs} className='mobile-logo' alt='react_Logo' />
@@ -28,15 +25,13 @@ const Home = () => {
             </div>
             <div className='right'>
                 <div className='propic' >
-                    <motion.img src='https://ik.imagekit.io/ghelowee/Portfolio/propic_glitched.webp?updatedAt=1699032604863' alt='profile_picture' variants={fadeAnim(1)}
-                        initial='initial'
-                        animate='animate' />
-                    <motion.div className='power' variants={fadeAnim(1.5)}
-                        initial='initial'
-                        animate='animate'>
+                    <img src='https://ik.imagekit.io/ghelowee/Portfolio/propic_glitched.webp?updatedAt=1699032604863' alt='profile_picture' variants={fadeAnim(1)}
+                        />
+                    <div className='power' variants={fadeAnim(1.5)}
+                        >
                         <p>Powered by</p>
                         <img src={ImageLinks.reactjs} alt='react Logo' />
-                    </motion.div>
+                    </div>
                 </div>
             </div>
         </div>
